18558464e4b577db38bbac54bcac3f84fdeea780
Edgar Allan Fojas <edgar@circles.asia>
2021-05-05 19:16:44 +0800

    feat: implemented Alpha requirements  of CL tag trigger utility

--- step.sh
+++ step.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
-
-TAG=${bitrise_git_tag}
-
-echo "Routing received tag trigger..."
+GIT_TAG=${bitrise_git_tag}
 echo "------------------------"
-echo "TAG: ${TAG}"
-echo "OPTION: ${OPTION}"
+echo "Routing received tag trigger - ${GIT_TAG}"
 echo "------------------------"
 
-if [ -z "$TAG" ]; then
+SUPPORTED_COUNTRIES=${supported_countries}
+TAG_REGEX="-(AS|RC[0-9]+)-(${SUPPORTED_COUNTRIES})$"
+
+if [ -z "$GIT_TAG" ]; then
     echo "Error: BITRISE_GIT_TAG environment variable not found"
     exit 1
-elif [[ $TAG =~ -(AS|RC[0-9]+)-(SG|ID|TW|AU|JP)$ ]]; then
+elif [[ $GIT_TAG =~ $TAG_REGEX ]]; then
     COUNTRY="${BASH_REMATCH[2]}"
     COUNTRY=`echo "${COUNTRY}" | tr '[:upper:]' '[:lower:]'`
     TAG_TYPE="${BASH_REMATCH[1]}"
@@ -25,10 +22,10 @@ elif [[ $TAG =~ -(AS|RC[0-9]+)-(SG|ID|TW|AU|JP)$ ]]; then
         FLAVOR="${COUNTRY}.qa"
     fi
     
-    envman add --key FASTLANE_ENV --value "${FLAVOR}"
+    envman add --key TAG_ENVIRONMENT --value "${FLAVOR}"
     exit 0
 else
-    echo "Error: invalid input tag/option"
+    echo "Error: Invalid Tag - ${GIT_TAG}"
     exit 1
 fi
 
