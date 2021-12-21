0515a8a5a52ca95f3f1ac820e1f53a018642b9df
thm14 <desilva.mahesh@gmail.com>
2021-12-21 18:04:58 +0530

    feat: add new environment variables

--- step.sh
+++ step.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 set -ex
 
-GIT_DESTINATION_BRANCH=${bitrise_dest_branch}
+
+# getTargetUnderTest: takes in country as input and sets enbv relevant TARGET_UNDER_TEST value
+function setTargetEnvVars() {
+  COUNTRY=$1
+  TEST_TARGET_PREFIX="Selfcare"
+  TEST_APP_COUNTRY_PREFIX="APP_COUNTRY_"
+  case "${COUNTRY}" in
+    "au")
+      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Australia"
+      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}AUSTRALIA"
+      return 0
+      ;;
+    "sg")
+      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}"
+      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}SINGAPORE"
+      return 0
+      ;;
+    "tw")
+      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Taiwan"
+      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}TAIWAN"
+      return 0
+      ;;
+    "id")
+      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Indonesia"
+      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}INDONESIA"
+      return 0
+      ;;
+    *)
+      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}"
+      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}SINGAPORE"
+      return 0
+      ;;
+  esac
+}
+
+GIT_DESTINATION_BRANCH=$1
 echo "------------------------"
 echo "Routing received PR trigger - ${GIT_DESTINATION_BRANCH}"
 echo "------------------------"
 
-SUPPORTED_COUNTRIES=${supported_countries}
+SUPPORTED_COUNTRIES=$2
 SUPPORTED_COUNTRIES_LOWERCASE=`echo "${SUPPORTED_COUNTRIES}" | tr '[:upper:]' '[:lower:]'`
 TAG_REGEX="(${SUPPORTED_COUNTRIES_LOWERCASE})-master"
 
@@ -15,12 +50,12 @@ if [ -z "$GIT_DESTINATION_BRANCH" ]; then
     exit 1
 elif [[ $GIT_DESTINATION_BRANCH =~ $TAG_REGEX ]]; then
     COUNTRY="${BASH_REMATCH[1]}"
-    COUNTRY=`echo "${COUNTRY}" | tr '[:upper:]' '[:lower:]'`
     FLAVOR="${COUNTRY}.qa"
 
     envman add --key PR_UNIT_TEST_ENVIRONMENT --value "${FLAVOR}"
+    setTargetEnvVars $COUNTRY
     exit 0
 else
-    echo "Error: Invalid Tag - ${GIT_DESTINATION_BRANCH}"
+    echo "Error: Invalid Branch - ${GIT_DESTINATION_BRANCH}"
     exit 1
 fi
