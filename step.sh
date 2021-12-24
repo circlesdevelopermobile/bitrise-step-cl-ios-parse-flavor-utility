b4e9d3e659c85dcd2852ab8985886520a545c2fb
Mahesh De Silva <mahesh.desilva@circles.asia>
2021-12-24 14:23:55 +0530

    feat: add functionality to get flavor from destination branch name

--- step.sh
+++ step.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 set -ex
 
-
-# getTargetUnderTest: takes in country as input and sets enbv relevant TARGET_UNDER_TEST value
-function setTargetEnvVars() {
-  COUNTRY=$1
-  TEST_TARGET_PREFIX="Selfcare"
-  TEST_APP_COUNTRY_PREFIX="APP_COUNTRY_"
-  case "${COUNTRY}" in
-    "au")
-      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Australia"
-      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}AUSTRALIA"
-      return 0
-      ;;
-    "sg")
-      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}"
-      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}SINGAPORE"
-      return 0
-      ;;
-    "tw")
-      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Taiwan"
-      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}TAIWAN"
-      return 0
-      ;;
-    "id")
-      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}-Indonesia"
-      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}INDONESIA"
-      return 0
-      ;;
-    *)
-      envman add --key TARGET_UNDER_TEST --value "${TEST_TARGET_PREFIX}"
-      envman add --key TARGET_APP_COUNTRY --value "${TEST_APP_COUNTRY_PREFIX}SINGAPORE"
-      return 0
-      ;;
-  esac
-}
-
 GIT_DESTINATION_BRANCH=${bitrise_dest_branch}
 echo "------------------------"
 echo "Routing received PR trigger - ${GIT_DESTINATION_BRANCH}"
@@ -53,7 +18,6 @@ elif [[ $GIT_DESTINATION_BRANCH =~ $TAG_REGEX ]]; then
     FLAVOR="${COUNTRY}.qa"
 
     envman add --key PR_UNIT_TEST_ENVIRONMENT --value "${FLAVOR}"
-    setTargetEnvVars $COUNTRY
     exit 0
 else
     echo "Error: Invalid Branch - ${GIT_DESTINATION_BRANCH}"
